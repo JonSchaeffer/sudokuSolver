58f9db52ee2c001a46982927eafbd32e3638b30b
JonSchaeffer <schaefferj@allegheny.edu>
2020-06-09 23:09:42 -0400

    Built basic board from a 2D array.

---
+++ board.java
@@ -0,0 +1,8 @@
+public class board extends sudokuSolver {
+
+    public static int[][] createBoard() {
+        int[][] sudokuBoard = new int[8][8];
+        return sudokuBoard;
+    }
+
+}
--- sudokuSolver.java
+++ sudokuSolver.java
@@ -1,5 +1,12 @@
 public class sudokuSolver {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int[][] sudokuBoard = board.createBoard();
+
+        sudokuBoard[0][0] = 1;
+
+        System.out.println(sudokuBoard[0][0]);
     }
 }
